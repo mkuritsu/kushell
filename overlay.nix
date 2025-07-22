2d096fe1bbe1c2d36e73b538249515591d6de6ac
mkuritsu <rodrigoalexccorreia@gmail.com>
2025-07-22 12:23:16 +0100

    disabled X11 and I3 quickshell modules

--- overlay.nix
+++ overlay.nix
@@ -1,3 +1,6 @@
 inputs: final: prev: {
-  quickshell = inputs.quickshell.packages.${final.system}.default;
+  quickshell = inputs.quickshell.packages.${final.system}.default.override {
+    withX11 = false;
+    withI3 = false;
+  };
 }
